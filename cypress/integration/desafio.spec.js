de1e1575c68ce8a0b358c947219a93843f0482df
Gabriele Wirth <wirthgabrielew@gmail.com>
2021-08-24 18:35:42 -0300

    Segundo commit

--- cypress/integration/desafio.spec.js
+++ cypress/integration/desafio.spec.js
@@ -20,22 +20,28 @@ describe("Desafio Web", () => {
         
         Cadastro.acessarCadastro();
         Cadastro.preencherCadastro(name, email, password);
+
+        cy.get('.alert-link').should('have.text', "Cadastro realizado com sucesso");
         
-        cy.get('[data-testid=logout]').should('have.text', "Logout");
+        
+        // cy.get('[data-testid=logout]').should('have.text', "Logout");
 
     });
 
-    it("Adicionar um produto a Lista e Validar a existência deste produto na Lista de Compras", () => {
-               
-        Login.acessarLogin();
-        Login.preencherLogin(email, password);
-        
+    it("Adicionar um produto a Lista e Validar", () => {
+           
         cy.get('h1').should('have.text', "Serverest Store");
         cy.get(':nth-child(1) > .card-body > div > [href="/minhaListaDeProdutos"] > [data-testid=adicionarNaLista]').click();
+
+
+    }); 
+
+    it("Validar a existência deste produto na Lista de Compras", () => {
+                      
         cy.get('h1').should('have.text', "Lista de Compras");
         cy.get('[data-testid=shopping-cart-product-name]').should('be.visible');
 
-    });
+    }); 
 
 });
 
