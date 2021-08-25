e3880e1ccafadaa46b33b58af4b6ee08e3ef8c0a
Gabriele Wirth <wirthgabrielew@gmail.com>
2021-08-24 21:13:08 -0300

    Terceiro commit

--- cypress/integration/desafio.spec.js
+++ cypress/integration/desafio.spec.js
@@ -7,7 +7,7 @@
 // - Validar a existência deste produto na Lista de Compras
 
 import Cadastro  from "../support/Cadastro";
-import Login from "../support/Login";
+
 
 var faker = require('faker');
 
@@ -16,32 +16,17 @@ var faker = require('faker');
     var password = faker.internet.password();
 
 describe("Desafio Web", () => {
-    it("Criar um novo usuário não sendo administrador", () => {
+    it("Adicionar um produto a Lista e Validar", () => {
         
         Cadastro.acessarCadastro();
         Cadastro.preencherCadastro(name, email, password);
 
-        cy.get('.alert-link').should('have.text', "Cadastro realizado com sucesso");
-        
-        
-        // cy.get('[data-testid=logout]').should('have.text', "Logout");
-
-    });
-
-    it("Adicionar um produto a Lista e Validar", () => {
-           
-        cy.get('h1').should('have.text', "Serverest Store");
         cy.get(':nth-child(1) > .card-body > div > [href="/minhaListaDeProdutos"] > [data-testid=adicionarNaLista]').click();
-
-
-    }); 
-
-    it("Validar a existência deste produto na Lista de Compras", () => {
-                      
-        cy.get('h1').should('have.text', "Lista de Compras");
         cy.get('[data-testid=shopping-cart-product-name]').should('be.visible');
 
-    }); 
+        cy.get('[data-testid=limparLista]').click();
+
+    });
 
 });
 
--- cypress/support/Login/elements.js
+++
@@ -1,5 +0,0 @@
-export const ELEMENTS = {
-    email: '[data-testid=email]',
-    senha: '[data-testid=senha]',
-    btnEntrar: '[data-testid=entrar]'
-};
